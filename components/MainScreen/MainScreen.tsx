264c22662af2df30d7e7e4f48d8bcf99ffaba71a
SzortykaZSK <mateusz.szortyka@uczen.zsk.poznan.pl>
2024-06-17 12:30:02 +0200

    working on mainScreen

--- components/MainScreen/MainScreen.tsx
+++ components/MainScreen/MainScreen.tsx
@@ -4,6 +4,7 @@ import { Dimensions, StyleSheet, SafeAreaView, View, Text, Modal, TextInput, Tou
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Header from './MainScreenComponents/Header';
 import TopSection from './MainScreenComponents/TopSection';
+import ConnectButton from '../SetUpScreen/SetUpScreenComponents/ConnectButton';
 
 
 function MainScreen({pageSwitcher}: any) {
@@ -14,6 +15,29 @@ function MainScreen({pageSwitcher}: any) {
     const [APIKey, setAPIKey] = useState<string>('')
     const [SSLCertificate, setSSLCertificate] = useState<boolean>(false)
 
+    useEffect(()=>{
+        validateForm()
+    }, [webSiteURL, APIKey])
+
+    const validateForm = () =>{
+        
+    }
+
+    const storeData = async (key:string, value:string) => {
+        try {
+          await AsyncStorage.setItem(key, value);
+        } catch (error) {
+          console.error('Error storing data', error);
+        }
+      };
+
+    const addConnectionHandle = async() =>{
+        setIsVisible(false)
+        let url = (SSLCertificate?'https://':'http://')+webSiteURL
+        await storeData(url, APIKey)
+        setIsConfigured(true)
+        setTimeout(()=>{pageSwitcher('Main')}, 6000)
+    }
     
   return (
     <SafeAreaView
@@ -22,11 +46,63 @@ function MainScreen({pageSwitcher}: any) {
         <ScrollView
             style={styles.scroll}
         >
+            <Modal
+            visible={isVisible}
+            animationType="slide"
+            transparent
+            style={styles.modal}
+            onRequestClose={()=>{
+                setIsVisible(false)
+            }
+            }
+        >
+            <View
+                style={styles.modalContentContainer}
+            >
+                <View
+                    style={styles.content}
+                >
+                    <Text style={styles.labelText}>Your WordPress Website Domein</Text>
+                    <TextInput 
+                        placeholder={'example.com'}
+                        value={webSiteURL} 
+                        onChangeText={setWebSiteURL} 
+                        style={styles.input} 
+                    />
+                    <Text style={styles.labelText}>Generated API Key</Text>
+                    <TextInput 
+                        placeholder={'FZphWtR...'}
+                        value={APIKey} 
+                        onChangeText={setAPIKey} 
+                        style={styles.input} 
+                    />
+                    <View
+                        style={styles.checkboxContainer}
+                    >
+                        <Text
+                            style={{fontSize:18, width:'70%', flexWrap:'wrap', color:'#1C244B', fontWeight:'700'}}
+                        >
+                        The website has an SSL certificate
+                        </Text>
+                    <TouchableOpacity
+                        style={{width:40, height:40, borderRadius:10, borderWidth:2, borderColor:'#9952D8', padding:2,}}
+                        onPress={()=>{
+                            setSSLCertificate(!SSLCertificate)
+                        }}
+                    >
+                        <View style={{backgroundColor: SSLCertificate?'#9952D8':'#fff', width:'100%', height:'100%', borderRadius:10}}></View>
+                    </TouchableOpacity>
+                    </View>
+                    
+                    <ConnectButton pressEventHandler={()=>{addConnectionHandle()}} />
+                </View>
+            </View>
+        </Modal>
             <View
                 style={styles.mainContainer}
             >
                 <Header />
-                <TopSection />
+                <TopSection pressEventHandler={()=>{setIsVisible(true)}}/>
             </View>
         </ScrollView>
     </SafeAreaView>
@@ -46,6 +122,70 @@ const styles = StyleSheet.create({
         width:'100%',
 
     },
+    labelText:{
+        fontSize:20,
+        fontWeight:'700',
+        color:'#1C244B',
+        padding:20,
+    },
+    modal:{
+        width:'100%',
+        height:'100%',
+        
+    },
+    modalContentContainer:{
+        width:'100%',
+        height:'100%',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'flex-end',
+        backgroundColor:'transparent'
+    },
+    content:{
+        borderTopLeftRadius:50,
+        borderTopRightRadius:50,
+        backgroundColor:'#fff',
+        width:'100%',
+        height:'60%',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'flex-start',
+        padding:20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+
+        elevation: 24,
+    },
+    input:{
+        width:'90%',
+        height:60,
+        borderRadius:10,
+        backgroundColor:'#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+                width: 0,
+                height: 0,
+        },
+        shadowOpacity: 0.44,
+        shadowRadius: 10.32,
+
+        elevation: 16,
+        fontSize:18, 
+        paddingLeft:10,
+    },
+    checkboxContainer:{
+        width:'100%',
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-around',
+        padding:20,
+    }
 })
 
 
--- components/MainScreen/MainScreenComponents/TopSection.tsx
+++ components/MainScreen/MainScreenComponents/TopSection.tsx
@@ -1,25 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Dimensions } from 'react-native';
 import { View, Text, SafeAreaView, StyleSheet, ImageBackground, Image, TouchableOpacity, Platform } from 'react-native';
-import { Octicons } from '@expo/vector-icons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+interface TopSectionProps{
+    pressEventHandler: any
+}
 
-function TopSection() {
-    const [keys, setKeys] = useState<any>([]);
 
-    useEffect(() => {
-        const fetchKeys = async () => {
-          try {
-            const allKeys = await AsyncStorage.getAllKeys();
-            setKeys(allKeys);
-          } catch (e) {
-            console.error(e);
-          }
-        };
-    
-        fetchKeys();
-      }, []);
+function TopSection({pressEventHandler}: TopSectionProps) {
 
   return (
     <View
@@ -31,10 +22,24 @@ function TopSection() {
             <ImageBackground
                 source={require('./../../../assets/background-texture.jpg')}
                 style={styles.imgBackground}
-                imageStyle={styles.imgBackground}
+                imageStyle={{borderRadius:20, width:'100%'}}
                 blurRadius={40}
             >
-
+                <Text
+                    style={styles.whiteText}
+                >
+                    Manage all your wordpress websites 
+                </Text>
+                <View
+                    style={{flexDirection:'row', justifyContent:'space-between', paddingRight:20, paddingLeft:20, paddingBottom:20,}}
+                >
+                    <Text
+                        style={styles.smallText}
+                    >
+                        Go to list
+                    </Text>
+                    <AntDesign name="arrowright" size={24} color="white"/>
+                </View>
             </ImageBackground>
         </TouchableOpacity>
         <View
@@ -43,17 +48,25 @@ function TopSection() {
             <TouchableOpacity
                 style={[styles.smallBox, {backgroundColor:'#000'}]}
             >
-                
+                <Text
+                    style={[styles.whiteText, {textAlign:'center'}]}
+                >
+                    Web statistics
+                </Text>
+                <Ionicons name="stats-chart-outline" size={50} color="#425FFC" style={{textAlign:'center'}}/>    
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.smallBox}
+                onPress={()=>{pressEventHandler()}}
             >
-                
+                <Text
+                    style={styles.blackText}
+                >
+                    Connect another website
+                </Text>
+                <AntDesign name="pluscircle" size={35} color="black" style={{textAlign:'center'}}/>
             </TouchableOpacity>
         </View>
-        {keys.map((key:any, index:any) => (
-        <Text key={index}>{key}</Text>
-      ))}
     </View>
   );
 }
@@ -86,6 +99,9 @@ const styles = StyleSheet.create({
         width:'100%',
         height:'100%',
         borderRadius:20,
+        display:'flex',
+        alignContent:'flex-start',
+        justifyContent:'flex-end',
     },
     smallBoxesContainer:{
         width:'100%',
@@ -100,8 +116,29 @@ const styles = StyleSheet.create({
         height:190,
         borderRadius:20, 
         borderWidth:1,
-        borderColor:'#000'
+        borderColor:'#000',
+        display:"flex",
+        justifyContent:'center',
+        alignContent:'center',
     },
+    whiteText:{
+        margin:20,
+        color:'#fff',
+        fontSize:25,
+        fontWeight:'700'
+    },
+    smallText:{
+        color:'#fff',
+        fontSize:18,
+        fontWeight:'900'
+    },      
+    blackText:{
+        margin:20,
+        color:'#000',
+        fontSize:25,
+        fontWeight:'700',
+        textAlign:'center',
+    }
 })
 
 
