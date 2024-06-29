06a75f7048c9a0260c727df2e7660f3d78f54e8f
SzortykaZSK <mateusz.szortyka@uczen.zsk.poznan.pl>
2024-06-29 19:18:50 +0200

    create manager screen

--- components/ManagerScreen/ManagerScreen.tsx
+++ components/ManagerScreen/ManagerScreen.tsx
@@ -2,9 +2,10 @@ import LottieView from 'lottie-react-native';
 import React, { useContext, useEffect, useState } from 'react';
 import { Dimensions, StyleSheet, SafeAreaView, View, Text, Modal, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import ConnectButton from '../SetUpScreen/SetUpScreenComponents/ConnectButton';
 import Footer from '../Footer/Footer';
 import Header from '../MainScreen/MainScreenComponents/Header';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 
 
@@ -36,12 +37,50 @@ function ManagerScreen({pageSwitcher, url}: any) {
                 style={styles.mainContainer}
             >
                 <Header text={'Website Manager'}/>
-                <Text>
+                <Text
+                    style={styles.titleText}
+                >
                     {webSiteURL.split('//')[1]}
                 </Text>
-                <Text>
-                    {APIKey}
-                </Text>
+                <View
+                    style={styles.button}
+                >
+                    <Text
+                        style={styles.buttonText}
+                    >
+                        Add new post 
+                    </Text>
+                    <MaterialIcons name="post-add" size={32} color="#9952D8" />
+                </View>
+                <View
+                    style={styles.button}
+                >
+                    <Text
+                        style={styles.buttonText}
+                    >
+                        Edit post 
+                    </Text>
+                    <Feather name="edit" size={24} color="#9952D8" />
+                </View>
+                <View
+                    style={styles.button}
+                >
+                    <Text
+                        style={styles.buttonText}
+                    >
+                        Edit post 
+                    </Text>
+                    <MaterialIcons name="delete-sweep" size={32} color="red" />
+                </View>
+                <View
+                    style={styles.statsContainer}
+                >
+                    <Text
+                        style={styles.statsText}
+                    >
+                        Stats
+                    </Text>
+                </View>
             </View>
         </ScrollView>
         <Footer pageSwitcher={pageSwitcher} />
@@ -125,6 +164,48 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'space-around',
         padding:20,
+    },
+    titleText:{
+        fontSize:22,
+        fontWeight:'700',
+        color:'gray'
+    },
+    button:{
+        width:'90%',
+        height:80,
+        borderRadius:20,
+        backgroundColor:'#fff',
+        shadowColor: "#000",
+        shadowOffset: {
+                width: 0,
+                height: 0,
+        },
+        shadowOpacity: 0.44,
+        shadowRadius: 10.32,
+
+        elevation: 16,
+        marginTop:20,
+        marginBottom:20,
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-around'
+    },
+    buttonText:{
+        fontSize:20,
+        fontWeight:'700'
+    },
+    statsContainer:{
+        width:'100%',
+        padding:20,
+        display:'flex',
+        alignItems:'flex-start',
+    },
+    statsText:{
+        fontSize:22,
+        fontWeight:'700',
+        padding:20,
+        color:'gray',
     }
 })
 
